feb135aef78f011117322e86889ffd19382db906
trnyc <turanyuce@gmail.com>
2022-04-21 16:10:14 +0300

    work two is done

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("EU 8 learning git and github");
 
     }
 
