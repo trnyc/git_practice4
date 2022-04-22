81053e8ec20f51ffd8934da681259fc9004e462f
trnyc <turanyuce@gmail.com>
2022-04-22 13:11:33 +0300

    Creatin a conflict in TestCase003 in master

---
+++ src/hello/TestCase003.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class TestCase003 {
+
+//    this is unknown chacnges to feature branch
+//    that exists in master,
+//    now git will not know what to do with this
+//    when we merge and canflict will occure
+}
